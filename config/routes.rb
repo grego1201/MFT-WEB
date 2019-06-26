63c456b19a0fcaea6608242eea63a2daaf0c918b
grego1201 <gregoriopatinoesteo@gmail.com>
2019-06-26 23:10:18 +0200

    Add fill_fencer route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   post 'guided/index', to: 'guided#obtain_decision'
   #get 'guided/results', to: 'guided#show_results'
   get 'fill_fencer/index'
+  post 'fill_fencer/index', to: 'fill_fencer#obtain_decision'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #
