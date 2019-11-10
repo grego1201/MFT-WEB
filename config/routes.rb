13d71f7a3f9724c87e4e8735ebad8ed18d9fff1f
grego1201 <gregoriopatinoesteo@gmail.com>
2019-11-10 20:49:51 +0100

    Add translations to training page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  scope "(:locale)", locale: /es|en/ do
+  scope '(:locale)', locale: /es|en/ do
     get 'guided/index'
     get 'guided/results'
     post 'guided/index', to: 'guided#obtain_decision'
-    #get 'guided/results', to: 'guided#show_results'
     get 'fill_fencer/index'
     get 'fill_fencer/results'
     post 'fill_fencer/index', to: 'fill_fencer#obtain_decision'
@@ -12,7 +11,9 @@ Rails.application.routes.draw do
     get 'show_suggests/index'
     post 'show_suggests/index', to: 'show_suggests#create_suggest'
     get 'training/index'
-    post 'training/index', to: 'training#show_random'
+    post 'training/index', to: 'training#calculate_random'
+    get 'training/show_random'
+    post 'training/show_random', to: 'training#calculate_random'
 
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     #
