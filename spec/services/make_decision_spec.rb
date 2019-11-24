517933fe3ce946d6c065b29339fd026abf472194
grego1201 <gregoriopatinoesteo@gmail.com>
2019-11-24 13:04:33 +0100

    Add test cases

--- spec/services/make_decision_spec.rb
+++ spec/services/make_decision_spec.rb
@@ -1,8 +1,6 @@
 require 'rails_helper'
 
 describe MakeDecision do
-  # Test cases can check at documentation
-  # grip 1 -> french
 
   let(:test_cases_data) { JSON.parse(File.open('spec/services/make_decision_test_cases_data').read) }
 
@@ -29,18 +27,18 @@ describe MakeDecision do
   end
 
   def fencers_to_params(fencer1, fencer2)
-   {
-      :fencer1=> {
-        :age=> fencer1.age.to_s,
-        :intimidated=> fencer1.intimidated ? 1 : 0,
-        :height=> fencer1.height.to_s,
-        :grip=> fencer1.grip == 1 ? "french" : "anatomic"
+    {
+      fencer1: {
+        age: fencer1.age.to_s,
+        intimidated: fencer1.intimidated ? 1 : 0,
+        height: fencer1.height.to_s,
+        grip: fencer1.grip == 1 ? 'french' : 'anatomic'
       },
-      :fencer2=>{
-        :age=> fencer2.age.to_s,
-        :intimidated=> fencer2.intimidated ? 1 : 0,
-        :height=> fencer2.height.to_s,
-        :grip=> fencer2.grip == 1 ? "french" : "anatomic"
+      fencer2: {
+        age: fencer2.age.to_s,
+        intimidated: fencer2.intimidated ? 1 : 0,
+        height: fencer2.height.to_s,
+        grip: fencer2.grip == 1 ? 'french' : 'anatomic'
       }
     }
   end
